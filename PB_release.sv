4d4bc8ab807a474ce699c3f72485402e0dc1e3c7
dhondup2 <32646210+dhondup2@users.noreply.github.com>
2017-10-09 10:57:22 -0500

    hello

--- PB_release.sv
+++ PB_release.sv
@@ -1,1 +1,28 @@
+module PB_release (clk, rst_n, PB, released);
+
+input clk, rst_n, PB;
+output released;
+
+
+logic PB1;
+logic PB2;
+logic PB3;
+
+always_ff @ (posedge clk, negedge rst_n) begin
+  if (~rst_n) begin
+   PB1 <= 1'b1;
+   PB2 <= 1'b1;
+   PB3 <= 1'b1;
+end
+  else begin
+   PB1 <= PB;
+   PB2 <= PB1;
+   PB3 <= PB2;
+end
+
+end
+
+assign released = PB2 & (~PB3);
+
+endmodule
 
